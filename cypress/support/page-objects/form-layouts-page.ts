2c1b2f1cd72b408857de8f8ad26ada39611dc672
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-08-23 21:14:30 +0100

    Create form layouts page model

---
+++ cypress/support/page-objects/form-layouts-page.ts
@@ -0,0 +1,15 @@
+export class FormLayoutsPage {
+
+    submitInlineForm(name, email) {
+        cy.contains('nb-card', 'Inline form')
+            .find('form').then(form => {
+                cy.wrap(form).find('[placeholder="Jane Doe"]').type(name)
+                cy.wrap(form).find('[placeholder="Email"]').type(email)
+                cy.wrap(form).find('[type="checkbox"]').check({ force: true })
+                cy.wrap(form).submit()
+            })
+    }
+
+}
+
+export const onFormLayoutsPage = new FormLayoutsPage;
